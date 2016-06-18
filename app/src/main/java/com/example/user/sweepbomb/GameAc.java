a438d87cc7de711a856fdcff1c3785ba3936718c
abc <z5831013@gmail.com>
2016-06-18 20:58:27 +0800

    first game

--- app/src/main/java/com/example/user/sweepbomb/GameAc.java
+++ app/src/main/java/com/example/user/sweepbomb/GameAc.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class GameAc extends AppCompatActivity {
 
@@ -16,6 +18,15 @@ public class GameAc extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        LinearLayout layout;
+        layout = (LinearLayout) findViewById(R.id.gaLL);
+        for(int i=0;i<10;i++){
+            Button buyButton = new Button(this);
+            buyButton.setText("1");
+
+            layout.addView(buyButton);
+
+        }
 
     }
 
